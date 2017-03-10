426e921d56489e4ebb6e5530a5341974e60ab02b
umatoma <umatomakun@gmail.com>
2017-03-10 22:17:57 +0900

    use logger middleware

--- server/main.go
+++ server/main.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello Trunks!!")
 	})
