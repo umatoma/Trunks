1022322ef1c99eaa8c8572a733de5508efef9097
umatoma <umatomakun@gmail.com>
2017-03-11 14:26:54 +0900

    do attack process as background job

--- server/executer.go
+++ server/executer.go
@@ -5,8 +5,17 @@ import (
 	"log"
 	"os"
 	"time"
+	"sync"
+	"errors"
 
 	vegeta "github.com/tsenart/vegeta/lib"
+)
+
+var (
+	isExecuting = false
+	execFlagLock sync.Mutex
+
+	errNowExecuting = errors.New("attacker is executing now")
 )
 
 // AttackExecuter aggregates the instances to attack
@@ -17,20 +26,49 @@ type AttackExecuter struct {
 	duration time.Duration
 }
 
-// Attack execute vegeta attack
-func (executer *AttackExecuter) Attack(filename string) (error) {
+// RegisterAttack register vegeta attack job
+func (executer *AttackExecuter) RegisterAttack(filename string) (error) {
+	execFlagLock.Lock()
+	defer execFlagLock.Unlock()
+
+	if isExecuting {
+		return errNowExecuting
+	}
+
+	// do attack
+	isExecuting = true
+	go func() {
+		if err := executer.attack(filename); err != nil {
+			log.Println(err)
+		}
+		execFlagLock.Lock()
+		isExecuting = false
+		execFlagLock.Unlock()
+	}()
+
+	return nil
+}
+
+func (executer *AttackExecuter) attack(filename string) error {
 	out, err := os.Create(filename)
 	if err != nil {
 		return fmt.Errorf("error opening %s: %s", filename, err)
 	}
 	defer out.Close()
 
+	res := executer.attacker.Attack(executer.targeter, executer.rate, executer.duration)
 	enc := vegeta.NewEncoder(out)
-	for r := range executer.attacker.Attack(executer.targeter, executer.rate, executer.duration) {
-		log.Println(r)
-		if err = enc.Encode(r); err != nil {
-			return err
+
+	for {
+		select {
+		case r, ok := <-res:
+			if !ok {
+				return nil
+			}
+			if err = enc.Encode(r); err != nil {
+				return err
+			}
+			log.Println(r)
 		}
 	}
-	return nil
 }
