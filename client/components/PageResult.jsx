5ef0badd26da7bb7a52af0f827aaf40509087e32
umatoma <umatomakun@gmail.com>
2017-03-18 18:01:49 +0900

    display metrics on result page

--- client/components/PageResult.jsx
+++ client/components/PageResult.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ChartResults from './Charts/ChartResults';
+import Metrics from './Metrics';
 
 class PageResult extends React.Component {
   componentDidMount() {
@@ -46,6 +47,9 @@ class PageResult extends React.Component {
         <section className="section">
           <ChartResults results={detail.results} />
         </section>
+        <section className="section">
+          <Metrics metrics={detail.metrics} />
+        </section>
       </div>
     );
   }
@@ -58,7 +62,11 @@ PageResult.defaultProps = {
 PageResult.propTypes = {
   filename: React.PropTypes.string.isRequired,
   onMount: React.PropTypes.func.isRequired,
-  detail: React.PropTypes.object,
+  detail: React.PropTypes.shape({
+    isFetching: React.PropTypes.bool.isRequired,
+    results: React.PropTypes.array.isRequired,
+    metrics: React.PropTypes.object.isRequired,
+  }),
 };
 
 export default PageResult;
