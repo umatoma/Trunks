e683fe7dcb60f2ec551d1e95fcb2bd2df7c55dec
umatoma <umatomakun@gmail.com>
2017-03-20 14:28:13 +0900

    added histgram information to api response

--- server/report.go
+++ server/report.go
@@ -5,10 +5,12 @@ import (
 	"io"
 	"math"
 	"os"
+	"time"
 
 	vegeta "github.com/tsenart/vegeta/lib"
 )
 
+// ParseResultsFromFile generate vegeta.Results from file
 func ParseResultsFromFile(file *os.File) (*vegeta.Results, error) {
 	decoder := vegeta.NewDecoder(file)
 
@@ -34,54 +36,65 @@ func ParseResultsFromFile(file *os.File) (*vegeta.Results, error) {
 func NewJSONMultiReporter(rs *vegeta.Results) vegeta.Reporter {
 	return func(w io.Writer) error {
 		metrics := &vegeta.Metrics{}
+		histgram := &vegeta.Histogram{
+			Buckets: reportBuckets(),
+		}
 		results := make([]map[string]interface{}, rs.Len())
+
 		for i, r := range *rs {
 			metrics.Add(&r)
-			results[i] = map[string]interface{}{
-				"Code":            r.Code,
-				"Timestamp":       r.Timestamp,
-				"Latency":         r.Latency,
-				"BytesOut":        r.BytesOut,
-				"BytesIn":         r.BytesIn,
-				"Error":           r.Error,
-				"TimeShort":       r.Timestamp.Format("15:04:05"),
-				"ElapsedTime":     round(r.Timestamp.Sub((*rs)[0].Timestamp).Seconds(), 2),
-				"LatencyMilliSec": round(r.Latency.Seconds()*1000, 2),
-			}
+			histgram.Add(&r)
+			results[i] = resultRow(&r, rs)
 		}
 		metrics.Close()
+
+		histRows := make([]map[string]interface{}, len(histgram.Counts))
+		for j, count := range histgram.Counts {
+			histRows[j] = histRow(histgram, j, count)
+		}
+
 		return json.NewEncoder(w).Encode(map[string]interface{}{
-			"metrics": metrics,
-			"results": results,
+			"metrics":  metrics,
+			"results":  results,
+			"histgram": histRows,
 		})
 	}
 }
 
-// PlotReporterFactory make a vegeta.Repoter from file
-func PlotReporterFactory(file *os.File) (vegeta.Reporter, error) {
-	decoder := vegeta.NewDecoder(file)
-
-	var reporter vegeta.Reporter
-	var report vegeta.Report
-	var rs vegeta.Results
-	reporter, report = vegeta.NewPlotReporter("Vegeta Plot", &rs), &rs
-
-	for {
-		var r vegeta.Result
-		if err := decoder.Decode(&r); err != nil {
-			if err == io.EOF {
-				break
-			}
-			return nil, err
-		}
-		report.Add(&r)
+func reportBuckets() vegeta.Buckets {
+	return vegeta.Buckets{
+		0 * time.Second,
+		10 * time.Millisecond,
+		50 * time.Millisecond,
+		100 * time.Millisecond,
+		500 * time.Millisecond,
+		1000 * time.Millisecond,
 	}
+}
 
-	if c, ok := report.(vegeta.Closer); ok {
-		c.Close()
+func resultRow(r *vegeta.Result, rs *vegeta.Results) map[string]interface{} {
+	return map[string]interface{}{
+		"Code":            r.Code,
+		"Timestamp":       r.Timestamp,
+		"Latency":         r.Latency,
+		"BytesOut":        r.BytesOut,
+		"BytesIn":         r.BytesIn,
+		"Error":           r.Error,
+		"TimeShort":       r.Timestamp.Format("15:04:05"),
+		"ElapsedTime":     round(r.Timestamp.Sub((*rs)[0].Timestamp).Seconds(), 2),
+		"LatencyMilliSec": round(r.Latency.Seconds()*1000, 2),
 	}
+}
 
-	return reporter, nil
+func histRow(h *vegeta.Histogram, i int, count uint64) map[string]interface{} {
+	ratio := round(float64(count)/float64(h.Total), 2)
+	lo, hi := h.Buckets.Nth(i)
+	return map[string]interface{}{
+		"lo":    lo,
+		"hi":    hi,
+		"ratio": ratio,
+		"count": count,
+	}
 }
 
 func round(f float64, places int) float64 {
