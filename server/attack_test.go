7692e65e06f05cd69ef758f345a10be4b79aabbb
umatoma <umatomakun@gmail.com>
2017-03-22 23:34:33 +0900

    added unit tests for attack.go

--- server/attack_test.go
+++ server/attack_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	vegeta "github.com/tsenart/vegeta/lib"
 )
 
-type BroadcasterMock struct {}
+type BroadcasterMock struct{}
 
 func (m *BroadcasterMock) Broadcast(event string, data interface{}) error {
 	return nil
@@ -14,9 +16,10 @@ func (m *BroadcasterMock) Broadcast(event string, data interface{}) error {
 
 func TestGetAttackWorker(t *testing.T) {
 	opts := NewAttackOptions()
-	opts.Targets = "GET http://localhost:3000/"
+	opts.Targets = "GET http://localhost:3000/\nX-Trunks: xyz"
 	opts.Rate = 5
 	opts.Duration = "10s"
+	opts.Body = "abcdefg"
 
 	w, err := opts.GetAttackWorker(&BroadcasterMock{})
 	assert.Nil(t, err)
@@ -24,6 +27,13 @@ func TestGetAttackWorker(t *testing.T) {
 	assert.NotNil(t, w.targeter)
 	assert.Equal(t, w.rate, uint64(5))
 	assert.Equal(t, w.duration.String(), "10s")
+
+	var tgt vegeta.Target
+	w.targeter(&tgt)
+	assert.Equal(t, tgt.Method, "GET")
+	assert.Equal(t, tgt.URL, "http://localhost:3000/")
+	assert.Equal(t, tgt.Header, http.Header{"X-Trunks": []string{"xyz"}})
+	assert.Equal(t, tgt.Body, []byte("abcdefg"))
 }
 
 func TestTargets(t *testing.T) {
