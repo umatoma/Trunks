ffffe6fe5d3e8b97d8a6abbc04f1d3732f5c88c3
umatoma <umatomakun@gmail.com>
2017-03-26 18:59:33 +0900

    move AppState class definition to another file

---
+++ client/state/models/index.js
@@ -0,0 +1,58 @@
+import { Record, List, Map } from 'immutable';
+import AppState from './app-state';
+
+export const ModelImportOption = Record({
+  isModalActive: false,
+  error: null,
+  text: '',
+});
+
+export const ModelWorker = Record({
+  status: 'ready',
+  error: null,
+  duration: 0,
+  rate: 0,
+  filename: '',
+});
+
+export const ModelMetrics = Record({
+  bytes_in: { total: 0, mean: 0 },
+  bytes_out: { total: 0, mean: 0 },
+  duration: 0,
+  earliest: '',
+  end: '',
+  errors: null,
+  latencies: { mean: 0, max: 0, '50th': 0, '95th': 0, '99th': 0 },
+  latest: {},
+  rate: 0,
+  requests: 0,
+  status_codes: {},
+  success: 0,
+  wait: 0,
+});
+
+export const ModelReport = Record({
+  isFetching: true,
+  showResultList: false,
+  error: null,
+  metrics: new ModelMetrics(),
+  histgram: List(),
+  results: List(),
+});
+
+export const ModelFormAttack = Record({
+  Body: '',
+  Duration: '10s',
+  Rate: 1,
+  Targets: '',
+});
+
+export const getInitialState = state => Object.assign({
+  appState: new AppState(),
+  importOption: new ModelImportOption(),
+  worker: new ModelWorker(),
+  metrics: new ModelMetrics(),
+  resultFiles: List(),
+  reports: Map(),
+  formAttack: new ModelFormAttack(),
+}, state);
