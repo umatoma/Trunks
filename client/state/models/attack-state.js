2f7d4bdd881d6feb1e3c8340f56420c1b6e7e72d
umatoma <umatomakun@gmail.com>
2017-03-26 19:46:01 +0900

    add comments

--- client/state/models/attack-state.js
+++ client/state/models/attack-state.js
@@ -56,14 +56,29 @@ const AttackStateRecord = new Record({
 });
 
 export default class AttackState extends AttackStateRecord {
+  /**
+   * update importOption
+   * @param {Object} params
+   * @return {AttackState}
+   */
   updateModalImportOption(params) {
     return this.set('importOption', this.importOption.merge(params));
   }
 
+  /**
+   * set resultFiles
+   * @param {Array.<{Object}>}
+   * @return {AttackState}
+   */
   setResultFiles(files) {
     return this.set('resultFiles', List(files));
   }
 
+  /**
+   * set state to attack started
+   * @param {Object} workerParams
+   * @return {AttackState}
+   */
   startAttack(workerParams) {
     return this.merge({
       worker: new ModelWorker(Object.assign({ status: 'active', filename: '' }, workerParams)),
@@ -71,18 +86,37 @@ export default class AttackState extends AttackStateRecord {
     });
   }
 
+  /**
+   * set state to attack finished
+   * @param {String} filename
+   * @return {AttackState}
+   */
   finishAttack(filename) {
     return this.set('worker', this.worker.merge({ status: 'done', filename }));
   }
 
+  /**
+   * set state to attack canceled
+   * @return {AttackState}
+   */
   cancelAttack() {
     return this.set('worker', this.worker.set('status', 'canceled'));
   }
 
+  /**
+   * set state to attack failed
+   * @param {Error} error
+   * @return {AttackState}
+   */
   failAttack(error) {
     return this.set('worker', this.worker.merge({ status: 'error', error }));
   }
 
+  /**
+   * update metrics and set worker status to active
+   * @param {Object} metricsParams
+   * @return {AttackState}
+   */
   updateAttackMetrics(metricsParams) {
     return this.merge({
       worker: this.worker.set('status', 'active'),
@@ -90,10 +124,23 @@ export default class AttackState extends AttackStateRecord {
     });
   }
 
+  /**
+   * initialize the report data in the list
+   * @param {String} filename
+   * @return {AttackState}
+   */
   initReportData(filename) {
     return this.set('reports', this.reports.set(filename, new ModelReport()));
   }
 
+  /**
+   * set report data in the list
+   * @param {String} filename
+   * @param {Object} metrics
+   * @param {Array} results
+   * @param {Array} histgram
+   * @return {AttackState}
+   */
   setReportData(filename, metrics, results, histgram) {
     const reports = this.reports.update(filename, (d) => { // eslint-disable-line
       return d.merge({
@@ -106,6 +153,12 @@ export default class AttackState extends AttackStateRecord {
     return this.set('reports', reports);
   }
 
+  /**
+   * set error in the list
+   * @param {String} filename
+   * @param {Error} error
+   * @return {AttackState}
+   */
   setReportDataError(filename, error) {
     const reports = this.reports.update(filename, (d) => { // eslint-disable-line
       return d.merge({ isFetching: false, error });
@@ -113,6 +166,11 @@ export default class AttackState extends AttackStateRecord {
     return this.set('reports', reports);
   }
 
+  /**
+   * set report state to showing results
+   * @param {String} filename
+   * @return {AttackState}
+   */
   showResultList(filename) {
     const reports = this.reports.update(filename, (d) => { // eslint-disable-line
       return d.set('showResultList', true);
@@ -120,10 +178,20 @@ export default class AttackState extends AttackStateRecord {
     return this.set('reports', reports);
   }
 
+  /**
+   * update formAttack
+   * @param {Object} params
+   * @return {AttackState}
+   */
   updateFormAttack(params) {
     return this.set('formAttack', this.formAttack.merge(params));
   }
 
+  /**
+   * set formAttack
+   * @param {Object}
+   * @return {AttackState}
+   */
   setFormAttack(params) {
     return this.set('formAttack', new ModelFormAttack(params));
   }
